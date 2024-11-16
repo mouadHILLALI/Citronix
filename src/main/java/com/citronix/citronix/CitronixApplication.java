107b7bd4937a1553e6e0bcb6ff465ac3a770ccd3
mouadHILLALI <mauadtoto001@gmail.com>
2024-11-16 18:04:31 +0100

    first commit

---
+++ src/main/java/com/citronix/citronix/CitronixApplication.java
@@ -0,0 +1,13 @@
+package com.citronix.citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitronixApplication.class, args);
+	}
+
+}
